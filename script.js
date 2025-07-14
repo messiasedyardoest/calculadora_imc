1d4db676cea6ae89fc65e1ee5533596333ee4e89
Messias Eduardo <messiaseduardoest@gmail.com>
2025-07-14 17:00:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,26 +31,6 @@ else{
 }
 
 
-
-//function calc(){
-    //let num1 = parseFloat(document.getElementById('num1').value);
-    //let num2 = parseFloat(document.getElementById('num2').value);
-    //let operacao = document.getElementById('operacao').value;
-   // let resultado;
-
-    //switch(operacao){
-        //case '+':
-          //  resultado = (num1) + parseFloat(num2);
-         //   break;
-       // case '-':
-         //   resultado = num1 - num2;
-         //   break;
-      //  case '*':
-         //   resultado = num1 * num2;
-         //   break;
-        //case '/':
-         //   resultado = num2 !== 0 ? num1 / num2 : 'Erro: Divisão por zero!';
-        //    break;
         //default:
         //    resultado = 'Operação inválida!';
 //}   
